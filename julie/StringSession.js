1da8d6ee83db246d15f4407bdf7ec90f415fcc98
ramshad21 <100375225+ramshad21@users.noreply.github.com>
2022-03-01 22:46:07 +0400

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'LizaMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'ShaMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
